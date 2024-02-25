b301ccc116503ead2746342d8948706667139e2c
swapna <swapnap422@gmail.com>
2024-02-25 21:37:28 +0530

    commited

---
+++ mongodb.sh
@@ -0,0 +1,6 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+sed 's/127.0.0.1/0.0.0.0/g' etc/mongod.conf
+systemctl restart mongod
