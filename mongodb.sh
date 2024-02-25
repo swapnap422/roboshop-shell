0ca8bcdaaf0a08c8242392728e337554027900d3
swapna <swapnap422@gmail.com>
2024-02-25 22:02:04 +0530

    commited

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
